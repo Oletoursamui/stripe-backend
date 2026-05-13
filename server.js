8d7822566a1376102d259b9022442743289f7bf5
Oletoursamui <doomcycles81@gmail.com>
2026-05-13 12:45:01 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const Stripe = require('stripe');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.post('/crear-pago', async (req, res) => {
+  try {
+    const { amount } = req.body;
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: 'thb',
+            product_data: {
+              name: 'Reserva Tour',
+            },
+            unit_amount: Math.round(amount * 100),
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'https://tusitio.com/success',
+      cancel_url: 'https://tusitio.com/cancel',
+    });
+
+    res.json({ url: session.url });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error creando pago' });
+  }
+});
+
+app.listen(3000, () => console.log('Servidor corriendo'));
