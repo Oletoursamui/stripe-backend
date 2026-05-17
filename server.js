3f36ef2111104c64fc6b7df09892632b6c644a73
Oletoursamui <doomcycles81@gmail.com>
2026-05-17 08:03:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -104,4 +104,8 @@ app.post('/crear-pago', async (req, res) => {
 });
 
 
-app.listen(3000, () => console.log('Servidor corriendo'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Servidor corriendo en puerto', PORT);
+});
